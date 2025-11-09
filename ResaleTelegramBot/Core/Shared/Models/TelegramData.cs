d18f1bb016b15b6c138fcc972d6fc732136d292e
zitiret <xnncs@mail.ru>
2025-11-09 16:37:31 +0300

    added /start message handler

--- ResaleTelegramBot/Core/Shared/Models/TelegramData.cs
+++ ResaleTelegramBot/Core/Shared/Models/TelegramData.cs
@@ -4,4 +4,13 @@ public class TelegramData
 {
     public long Id { get; set; }
     public string Username { get; set; }
+
+    public static TelegramData Create(long id, string username)
+    {
+        return new TelegramData
+        {
+            Id = id,
+            Username = username
+        };
+    }
 }
--- ResaleTelegramBot/Telegram/UpdatesHandling/Handlers/Command/Implementation/StartCommandHandler.cs
+++ ResaleTelegramBot/Telegram/UpdatesHandling/Handlers/Command/Implementation/StartCommandHandler.cs
@@ -1,12 +1,33 @@
 namespace ResaleTelegramBot.Telegram.UpdatesHandling.Handlers.Command.Implementation;
 
 using Abstract;
+using Core.Shared.Models;
 using global::Telegram.Bot;
 using global::Telegram.Bot.Types;
-using Shared;
+using global::Telegram.Bot.Types.Enums;
+using global::Telegram.Bot.Types.ReplyMarkups;
+using Helpers.Abstract;
+using Helpers.Shared.Enums;
+using Services.Abstract;
+using Services.Contracts;
+using Shared.Enums;
+using Texts.Output;
+using User = Core.Models.User;
 
 public class StartCommandHandler : ICommandHandler
 {
+    private readonly IAuthService _authService;
+    private readonly ILogger<StartCommandHandler> _logger;
+    private readonly IReplyKeyboardGenerator _replyKeyboardGenerator;
+
+    public StartCommandHandler(ILogger<StartCommandHandler> logger, IReplyKeyboardGenerator replyKeyboardGenerator,
+                               IAuthService authService)
+    {
+        _logger = logger;
+        _replyKeyboardGenerator = replyKeyboardGenerator;
+        _authService = authService;
+    }
+
     public bool CanHandle(string command)
     {
         return command == CommandCodes.Start;
@@ -15,6 +36,32 @@ public class StartCommandHandler : ICommandHandler
     public async Task HandleCommandAsync(string args, Message message, ITelegramBotClient botClient,
                                          CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        if (message.From == null)
+        {
+            _logger.LogWarning("From is null");
+            return;
+        }
+
+        User? existingUser = await _authService.GetByTelegramIdAsync(message.From.Id, cancellationToken);
+        if (existingUser == null)
+        {
+            _logger.LogInformation("User with id: {userId} not found", message.From.Id);
+            User? created = await _authService.RegisterUserAsync(
+                UserRegistrationContract.Create(TelegramData.Create(message.From.Id, message.From.Username)),
+                cancellationToken);
+            if (created == null)
+            {
+                _logger.LogWarning("Failed to register user with id: {userId}", message.From.Id);
+                return;
+            }
+
+            _logger.LogInformation("User with id: {userId} successfully registered", created.Id);
+        }
+
+        ReplyMarkup keyboard = _replyKeyboardGenerator.GenerateKeyboardMarkup(KeyboardGenerationCodes.MainMenu);
+        string responseText = ResponseMessageStaticTexts.OnStart;
+
+        await botClient.SendMessage(message.From.Id, responseText, ParseMode.Html, replyMarkup: keyboard,
+            cancellationToken: cancellationToken);
     }
 }
