38b9a69a9e94db7f701724c529cd12c1f280c157
zitiret <xnncs@mail.ru>
2025-11-09 18:44:47 +0300

    added ordering to listings photos

--- ResaleTelegramBot/Core/Models/ListingPhoto.cs
+++ ResaleTelegramBot/Core/Models/ListingPhoto.cs
@@ -5,5 +5,6 @@ public class ListingPhoto
     public Guid Id { get; set; }
     public Listing Listing { get; set; }
     public string TelegramFileId { get; set; }
+    public int Order { get; set; }
     public DateTime AddedAt { get; set; }
 }
