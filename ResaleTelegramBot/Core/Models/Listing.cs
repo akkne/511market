b110631d6de5de59d6ac69b9213fcf96c3d63192
zitiret <xnncs@mail.ru>
2025-11-11 00:58:01 +0300

    added migration: added Price column to Listings table

--- ResaleTelegramBot/Core/Models/Listing.cs
+++ ResaleTelegramBot/Core/Models/Listing.cs
@@ -7,6 +7,7 @@ public class Listing
     public Guid Id { get; set; }
     public SellerProfile SellerProfile { get; set; }
     public string Title { get; set; }
+    public decimal Price { get; set; }
     public string Description { get; set; }
     public Category Category { get; set; }
     public ListingStatus Status { get; set; }
