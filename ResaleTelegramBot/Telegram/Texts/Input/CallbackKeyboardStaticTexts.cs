4f7618d95a7f5a3da7c9132de8d1d6ac159268f2
zitiret <xnncs@mail.ru>
2025-11-11 21:15:00 +0300

    updated ui/ux texts

--- ResaleTelegramBot/Telegram/Texts/Input/CallbackKeyboardStaticTexts.cs
+++ ResaleTelegramBot/Telegram/Texts/Input/CallbackKeyboardStaticTexts.cs
@@ -3,6 +3,6 @@ namespace ResaleTelegramBot.Telegram.Texts.Input;
 public class CallbackKeyboardStaticTexts
 {
     public static string OnListingPublicationConfirm = """
-                                                       Подтвердить
+                                                       ✅ Подтвердить
                                                        """;
 }
